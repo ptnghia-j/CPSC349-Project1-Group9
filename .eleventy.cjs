e240ebc141c28af57ae09d1f8ee8d0e96568936a
Nghia T Phan <81942140+ptnghia-j@users.noreply.github.com>
2022-10-03 00:22:16 -0700

    add tunnel for site collaboration

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false)
   eleventyConfig.setBrowserSyncConfig({
     notify: true,
-    open: true
+    open: true,
+    tunnel: true
   })
 }
